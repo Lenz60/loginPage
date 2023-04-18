e91c045cd332ac22f68ecf1067ca279de4de2aa0
Lenz60 <raflyandrian@gmail.com>
2023-04-19 03:34:10 +0700

    Added method to show data from decoded token in ProfileModel and show it at home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'home');
+$routes->get('/', 'HomeController::show');
+$routes->post('/', 'HomeController::index');
 $routes->get('/auth', 'AuthController');
 $routes->get('/login', 'AuthController::loginIndex');
 $routes->post('/login', 'AuthController::login');
---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+
+use App\Models\ProfileModel;
+use App\Controllers\BaseController;
+
+
+class HomeController extends BaseController
+{
+    public function __construct()
+    {
+        $this->model = new ProfileModel();
+    }
+    public function index()
+    {
+        // return view('home');
+        // 
+        // $decoded_token = JWT::decode($_COOKIE['COOKIE-SESSION'], new Key($key, 'HS256'));
+
+        return view('home');
+    }
+
+    public function show()
+    {
+        $token = $_COOKIE['COOKIE-SESSION'];
+        $model = new ProfileModel();
+        $result = $model->show($token);
+        print_r($result);
+    }
+}
